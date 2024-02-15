93a31d90abe3e556a0395bc454c590378542697a
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-15 12:45:37 +0530

    background colour for avatar in the login page changed

--- src/molecules/login.js
+++ src/molecules/login.js
@@ -3,16 +3,19 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import TextField from '@mui/material/TextField';
 
 const Login=() =>{
     const paperStyle={padding :20, height : '70vh', width:280,margin:"20px auto"}
+    const avatarStyle={backgroundColor:'blue'}
   return (
    <Grid>
 <Paper elevation ={10} style={paperStyle}>
   <Grid align='center'>
-  <Avatar> <LockOutlinedIcon /></Avatar>
+  <Avatar style={avatarStyle}> <LockOutlinedIcon /></Avatar>
   <h2>Sign in</h2>
   </Grid>
+  <TextField id="standard-basic" label="Username" variant="standard" placeholder="Enter your name" fullWidth/>
    
 
 </Paper>
