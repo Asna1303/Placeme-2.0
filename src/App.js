99e93bf02441fb5047fbd3c2d6c50e19d5233055
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-15 09:50:46 +0530

    created login.js and imported grid and paper and started designing  login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+import React from 'react';
 import './App.css';
-import Loginpage from './molecules/Loginpage'; // Corrected import statement with capital "L" in "Loginpage"
-import waves from "./images/waves.png";
+import Login from './molecules/login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Loginpage /> {/* Rendering the Loginpage component */}
-      </header>
+    
+     <Login/>
     </div>
   );
 }
 
 export default App;
+
 
---
+++ src/molecules/login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
+const Login=() =>{
+    const paperStyle={padding :20}
+  return (
+   <Grid>
+<Paper elevation ={10} style={paperStyle}>
+Sign in
+</Paper>
+   </Grid>
+  )
+}
+
+export default Login
