c518c7544beae84f98de8900a1fc145005ff720b
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-15 10:08:37 +0530

    added the lock avatar

--- src/molecules/login.js
+++ src/molecules/login.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 const Login=() =>{
-    const paperStyle={padding :20}
+    const paperStyle={padding :20, height : '70vh', width:280,margin:"20px auto"}
   return (
    <Grid>
 <Paper elevation ={10} style={paperStyle}>
+    <Avatar> <LockOutlinedIcon /></Avatar>
 Sign in
 </Paper>
    </Grid>
