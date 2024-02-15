1633fc35e37f0d73a5e4678bb450d85dd8e5d78d
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-15 13:14:53 +0530

    button added for sign in

--- src/molecules/login.js
+++ src/molecules/login.js
@@ -6,23 +6,26 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
 
 const Login=() =>{
     const paperStyle={padding :20, height : '70vh', width:280,margin:"20px auto"}
-    const avatarStyle={backgroundColor:'blue'}
+    const avatarStyle={backgroundColor:'green'}
   return (
    <Grid>
 <Paper elevation ={10} style={paperStyle}>
   <Grid align='center'>
   <Avatar style={avatarStyle}> <LockOutlinedIcon /></Avatar>
-  <h2>Sign in</h2>
+  <h2>Log in</h2>
   </Grid>
   <TextField id="standard-basic" label="Username*" variant="standard" placeholder="Enter your name" fullWidth/>
   <TextField id="standard-basic" label="Password*" variant="standard" placeholder="Enter your password" type='password' fullWidth/>
   <Grid container justifyContent="flex-start" alignItems="center">
                     <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me" />
                 </Grid>
-   
+  
+   <Button variant="contained" type='submit' color='primary' fullWidth>LOG IN</Button>
+
 
 </Paper>
    </Grid>
