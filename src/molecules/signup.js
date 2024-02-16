b9111f4e8120d4c1f3faa377b99fcd8bba4e184f
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-17 01:04:31 +0530

    created radio group button for gender

--- src/molecules/signup.js
+++ src/molecules/signup.js
@@ -5,6 +5,11 @@ import Avatar from '@mui/material/Avatar';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 import { Typography } from '@mui/material';
 
@@ -13,6 +18,7 @@ const Signup = () => {
   const avatarStyle = { backgroundColor: 'green' };
 const headerStyle={margin:0}
 const buttonStyle={margin:'8px 0'}
+const marginTop={marginTop:5}
   return (
     <Grid container justifyContent="center" alignItems="center">
       <Paper elevation={20} style={paperStyle}>
@@ -28,6 +34,19 @@ const buttonStyle={margin:'8px 0'}
         <TextField id="standard-basic" label="Username*" variant="standard" placeholder="Enter your name" fullWidth/>
         
   <TextField id="standard-basic" label="Email*" variant="standard" placeholder="Enter your mail id" fullWidth/>
+  <FormControl component="fieldset" style={marginTop}>
+  <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+  <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="female"
+    name="radio-buttons-group"
+    style={{display:'initial'}}
+  >
+    <FormControlLabel value="female" control={<Radio />} label="Female" />
+    <FormControlLabel value="male" control={<Radio />} label="Male" />
+   
+  </RadioGroup>
+</FormControl>
   
   <TextField id="standard-basic" label="Phone Number*" variant="standard" placeholder="Enter your phone number" fullWidth/>
   
