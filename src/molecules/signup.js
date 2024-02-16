c1717c14b998dd218bdbf468d41f1dfdfb79c44c
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-17 00:28:36 +0530

    created avatar and started designing signup page

--- src/molecules/signup.js
+++ src/molecules/signup.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
 
-function Signup() {
+import { Typography } from '@mui/material';
+
+const Signup = () => {
+  const paperStyle = { padding: 20, height: '70vh', width: 280, margin: '20px auto' };
+  const avatarStyle = { backgroundColor: 'green' };
+
   return (
-    <div>
-      <h1>signup ASNA</h1>
-    </div>
-  )
-}
+    <Grid container justifyContent="center" alignItems="center">
+      <Paper elevation={20} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            
+          </Avatar>
+          <h2>Sign up</h2>
+          <Typography variant='caption'>Please fill this form to create an account</Typography>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+};
 
-export default Signup
+export default Signup;
+
+
