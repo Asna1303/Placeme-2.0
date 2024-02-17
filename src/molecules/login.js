0f5ac434623d9b1b0bf2c7c83249970556c4197d
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-17 15:37:46 +0530

    initial values setted for formik

--- src/molecules/login.js
+++ src/molecules/login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
@@ -8,45 +8,85 @@ import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
-const Login=() =>{
-    const paperStyle={padding :20, height : '70vh', width:280,margin:"20px auto"}
-    const avatarStyle={backgroundColor:'green'}
-    const buttonStyle={margin:'8px 0'}
-  return (
-   <Grid>
-<Paper elevation ={10} style={paperStyle}>
-  <Grid align='center'>
-  <Avatar style={avatarStyle}> <LockOutlinedIcon /></Avatar>
-  <h2>Log in</h2>
-  </Grid>
-  <TextField id="standard-basic" label="Username*" variant="standard" placeholder="Enter your name" fullWidth/>
-  <TextField id="standard-basic" label="Password*" variant="standard" placeholder="Enter your password" type='password' fullWidth/>
-  <Grid container justifyContent="flex-start" alignItems="center">
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me" />
+import { Formik } from 'formik'; // Note the correct import statement
+
+const Login = () => {
+    const paperStyle = { padding: 20, height: '70vh', width: 280, margin: '20px auto' };
+    const avatarStyle = { backgroundColor: 'green' };
+    const buttonStyle = { margin: '8px 0' };
+
+    return (
+        <Grid container justifyContent="center">
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
+                    <h2>Log in</h2>
                 </Grid>
-  
-   <Button variant="contained" type='submit' style={buttonStyle} color='primary' fullWidth>LOG IN</Button>
-   <Grid container justifyContent="flex-start">
-<typography >
-<Link href="forgetpassword" >
- 
-  Forgot password
-</Link>
-</typography> 
-</Grid>
-<br></br>
-<Grid container justifyContent="flex-start">
-<typography>  Do you have an account?
-<Link href="signup" >
- Sign up!
- 
-</Link>
-</typography>
-</Grid>
-   
-</Paper>
-   </Grid>
-  )
+                <Formik
+                    initialValues={{ username: '', password: '', rememberMe: false }}
+                    onSubmit={(values) => {
+                        // Your form submission logic goes here
+                        console.log(values);
+                    }}
+                >
+                    {({ values, handleChange, handleSubmit }) => (
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                id="username"
+                                name="username"
+                                label="Username*"
+                                variant="standard"
+                                placeholder="Enter your name"
+                                fullWidth
+                                value={values.username}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                id="password"
+                                name="password"
+                                label="Password*"
+                                variant="standard"
+                                placeholder="Enter your password"
+                                type="password"
+                                fullWidth
+                                value={values.password}
+                                onChange={handleChange}
+                            />
+                            <Grid container justifyContent="flex-start" alignItems="center">
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            id="rememberMe"
+                                            name="rememberMe"
+                                            defaultChecked={values.rememberMe}
+                                            onChange={handleChange}
+                                        />
+                                    }
+                                    label="Remember me"
+                                />
+                            </Grid>
+                            <Button
+                                variant="contained"
+                                type="submit"
+                                style={buttonStyle}
+                                color="primary"
+                                fullWidth
+                            >
+                                LOG IN
+                            </Button>
+                        </form>
+                    )}
+                </Formik>
+                <Grid container justifyContent="flex-start">
+                    <Link href="forgetpassword">Forgot password</Link>
+                </Grid>
+                <br />
+                <Grid container justifyContent="flex-start">
+                    <p>Do you have an account? <Link href="signup">Sign up!</Link></p>
+                </Grid>
+            </Paper>
+        </Grid>
+    );
 }
 
-export default Login
+export default Login;
