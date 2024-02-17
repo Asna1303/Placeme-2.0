fb556202d554a25d332c8ebf033794c643d5d8dd
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-17 12:04:50 +0530

    cleared the error and login and signup page got on different pages:browserrouter should be given inside div in app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React from 'react';
 import './App.css';
-import Login from './molecules/login'
+import Login from './molecules/login';
 import Signup from './molecules/signup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
-
-
-import {
-  BrowserRouter, Routes, Route,
-} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-    
-     <Login/>
-     <BrowserRouter>
-     <Routes>
-     
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-     </Routes>
-     </BrowserRouter>
-     
-    </div>
+    <BrowserRouter> 
+      <div className="App">
+        <Routes> 
+          <Route path="/" element={<Login />} /> 
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
---
+++ src/molecules/forgetpassword.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Grid } from '@mui/material'
+const Forgetpassword=() =>{
+  return (
+    <Grid>
+        <h2>
+            asna password
+        </h2>
+    </Grid>
+  )
+}
+
+export default Forgetpassword
