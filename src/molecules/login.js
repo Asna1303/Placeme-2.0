c0a8a44a709cb0760b9269e4d0e589150472932a
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-19 09:55:36 +0530

    added background colour for login page

--- src/molecules/login.js
+++ src/molecules/login.js
@@ -13,70 +13,72 @@ import * as Yup from 'yup'
 
 const Login = ({ handleChange }) => {
 
-  const paperStyle = { padding: 20, height: '73vh', width: 300, margin: "0 auto" }
-  const avatarStyle = { backgroundColor: '#1bbd7e' }
-  const btnstyle = { margin: '8px 0' }
+  const containerStyle = { backgroundColor: '#1bbd7e', minHeight: '100vh', padding: '20px' };
+  const paperStyle = { padding: 20, height: '73vh', width: 300, margin: "0 auto" };
+  const avatarStyle = { backgroundColor: '#1bbd7e' };
+  const btnstyle = { margin: '8px 0' };
   const initialValues = {
       username: '',
       password: '',
       remember: false
-  }
+  };
   const validationSchema = Yup.object().shape({
       username: Yup.string().email('please enter valid email id').required("Required"),
       password: Yup.string().required("Required")
-  })
+  });
   const onSubmit = (values, props) => {
-      console.log(values)
+      console.log(values);
       setTimeout(() => {
-          props.resetForm()
-          props.setSubmitting(false)
-      }, 2000)
-
-  }
+          props.resetForm();
+          props.setSubmitting(false);
+      }, 2000);
+  };
+  
   return (
+    <div style={containerStyle}>
+      <Grid container justifyContent="center">
+          <Paper elevation={10} style={paperStyle}>
+              <Grid align='center'>
+                  <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
+                  <h2>Sign In</h2>
+              </Grid>
+              <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                  {(props) => (
+                      <Form>
+                        {console.log(props)}
+                          <Field as={TextField} label='Username' name="username"
+                              placeholder='Enter username' fullWidth required
+                              helperText={<ErrorMessage name="username" />}
+                          />
+                          <Field as={TextField} label='Password' name="password"
+                              placeholder='Enter password' type='password' fullWidth required
+                              helperText={<ErrorMessage name="password" />} />
+                          <Field as={FormControlLabel}
+                              name='remember'
+                              control={
+                                  <Checkbox
+                                      color="primary"
+                                  />
+                              }
+                              label="Remember me"
+                          />
+                          <Button type='submit' color='primary' variant="contained" disabled={props.isSubmitting}
+                              style={btnstyle} fullWidth>{props.isSubmitting ? "Loading" : "Sign in"}</Button>
 
-        <Grid container justifyContent="center">
-            <Paper elevation={10} style={paperStyle}>
-                <Grid align='center'>
-                    <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
-                    <h2>Sign In</h2>
-                </Grid>
-                <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                    {(props) => (
-                        <Form>
-                          {console.log(props)}
-                            <Field as={TextField} label='Username' name="username"
-                                placeholder='Enter username' fullWidth required
-                                helperText={<ErrorMessage name="username" />}
-                            />
-                            <Field as={TextField} label='Password' name="password"
-                                placeholder='Enter password' type='password' fullWidth required
-                                helperText={<ErrorMessage name="password" />} />
-                            <Field as={FormControlLabel}
-                                name='remember'
-                                control={
-                                    <Checkbox
-                                        color="primary"
-                                    />
-                                }
-                                label="Remember me"
-                            />
-                            <Button type='submit' color='primary' variant="contained" disabled={props.isSubmitting}
-                                style={btnstyle} fullWidth>{props.isSubmitting ? "Loading" : "Sign in"}</Button>
-
-                        </Form>
-                    )}
-                </Formik>
-                <Grid container justifyContent="flex-start">
-                    <Link href="forgetpassword">Forgot password</Link>
-                </Grid>
-                <br />
-                <Grid container justifyContent="flex-start">
-                    <p>Do you have an account? <Link href="signup">Sign up!</Link></p>
-                </Grid>
-            </Paper>
-        </Grid>
-    );
+                      </Form>
+                  )}
+              </Formik>
+              <Grid container justifyContent="flex-start">
+                  <Link href="forgetpassword">Forgot password</Link>
+              </Grid>
+              <br />
+              <Grid container justifyContent="flex-start">
+                  <p>Do you have an account? <Link href="signup">Sign up!</Link></p>
+              </Grid>
+          </Paper>
+      </Grid>
+    </div>
+  );
 }
 
 export default Login;
